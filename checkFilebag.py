2c666c2591a8e3cc58785da663bcb0af99102eae
Alexis Gallagher <alexis@alexisgallagher.com>
2020-07-29 12:58:54 -0700

    edits

--- checkFilebag.py
+++ checkFilebag.py
@@ -2,20 +2,22 @@
 
 import os,sys
 
-with open(sys.argv[1],'r') as f:
-    lines = [s.split("\t") for s in f.readlines()]
+def parseManifest(path):
+    with open(path,'r') as f:
+        lines = [s.split("\t") for s in f.readlines()]
+    (hashes,paths,fnames) = ([],[],[])
+    for line in lines:
+        hashes.append(line[0])
+        paths.append(line[1])
+        fnames.append(line[2])
+    return (hashes,paths,fnames)
 
+(h1,p1,f1) = parseManifest(sys.argv[1])
 
-(hashes,paths,fnames) = ([],[],[])
-for line in lines:
-    hashes.append(line[0])
-    paths.append(line[1])
-    fnames.append(line[2])
-
-print("entries: {}".format(len(lines)))
-print("unique fcontents: {}".format(len(set(hashes))))
-print("unique fnames: {}".format(len(set(fnames))))
-print("unique paths: {}".format(len(set(paths))))
+print("entries: {}".format(len(h1)))
+print("unique fcontents: {}".format(len(set(h1))))
+print("unique fnames: {}".format(len(set(f1))))
+print("unique paths: {}".format(len(set(p1))))
 
 
 
