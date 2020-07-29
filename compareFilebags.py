213570d19e0f2a9def0eea9d8c447276b2df43cf
Alexis Gallagher <alexis@alexisgallagher.com>
2020-07-29 12:41:29 -0700

    edits

--- compareFilebags.py
+++ compareFilebags.py
@@ -21,30 +21,36 @@ fs1 = set(f1)
 hs2 = set(h2)
 fs2 = set(f2)
 
+hsint = hs1.intersection(hs2)
 hs12 = hs1.issubset(hs2)
 hs21 = hs2.issubset(hs1)
 
-if hs12 and hs21:
+if len(hsint) == 0:
+    print("A and B have no file contents in common")
+elif hs12 and hs21:
     print("A and B have identical file contents")
 elif hs12:
     print("A's file contents is a subset of B's file contents")
 elif hs21:
     print("B's file contents is a subset of A's file contents")
 else:
-    print("No simple relationship between A and B's file conents")
+    print("No containment relationship between A and B's file contents.\n Intersction: {}\n Items: {}".format(len(hsint),sorted(list(hsint))))
 
+fsint = fs1.intersection(fs2)
 fs12 = fs1.issubset(fs2)
 fs21 = fs2.issubset(fs1)
 
-
-if fs12 and fs21:
+if len(fsint) == 0:
+    print("A and B have no file names in common")
+elif fs12 and fs21:
     print("A and B have identical file names")
 elif fs12:
     print("A's file names is a subset of B's file names")
 elif fs21:
     print("B's file names is a subset of A's file names")
 else:
-    print("No simple relationship between A and B's file names")
+    print("No containment relationship between A and B's file contents.\n Intersction: {}\n Items: {}".format(len(fsint),sorted(list(fsint))))
+
 
 
 
